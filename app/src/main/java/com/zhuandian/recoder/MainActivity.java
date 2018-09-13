29fe98863eec875fca3b9680ae864e9412ad5882
xiedong <zx381010>
2018-09-13 16:50:06 +0800

    添加仿QQ录音lib

--- app/src/main/java/com/zhuandian/recoder/MainActivity.java
+++ app/src/main/java/com/zhuandian/recoder/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.tfedu.record.IRecoderCallBack;
+import com.tfedu.record.RecoderPopupWindow;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,6 +19,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 startActivity(new Intent(MainActivity.this, com.tfedu.record.MainActivity.class));
+            }
+        });
+
+        findViewById(R.id.bth_qq_recoder).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecoderPopupWindow recoderPopupWindow = new RecoderPopupWindow(MainActivity.this);
+                recoderPopupWindow.setRecoderCallBack(new IRecoderCallBack() {
+                    @Override
+                    public void onSave(long length, String strLength, String voicePath) {
+                        //录音文件路径，上传服务器或者自定义处理
+                        System.out.println(voicePath + "------------");
+                    }
+                });
+                recoderPopupWindow.show();
             }
         });
 
